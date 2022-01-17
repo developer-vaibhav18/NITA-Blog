6aa756505fa654f2b232f060e2e2ac9db75b11c4
Vaibhav Kumar Sinha <67749908+devloper-vaibhav@users.noreply.github.com>
2022-01-18 02:16:29 +0530

    changes

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -23,14 +24,26 @@ const userSchema = new Schema({
   },
 
   name: {
-    firstName, secondName: {
+    firstName: {
+      type: String,
+      required: true
+    }, 
+    secondName: {
       type: String,
       required: true
     }
   },
 
   batch: {
-    startYear, endYear: {
+    startYear: {
+      type: Number,
+      required: true,
+      validate: [isYear = (v) => {
+        // need to modified
+        return true;
+      }, 'Invalid Year(enter like 2019)']
+    }, 
+    endYear: {
       type: Number,
       required: true,
       validate: [isYear = (v) => {
@@ -78,11 +91,26 @@ const userSchema = new Schema({
   }
 }, { timestamps: true });
 
-// creating model
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
 const User = mongoose.model('user', userSchema);
 
-// exporting model
-module.exports = {
-  User,
-}
+module.exports = User;
